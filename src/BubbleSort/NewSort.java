8afdfdac2c6187f74a1fa819d4acac3abffd2880
chand <chand@10.12.39.90>
2022-09-15 19:56:43 +0530

    NewSort

---
+++ src/BubbleSort/NewSort.java
@@ -0,0 +1,19 @@
+package BubbleSort;
+import java.util.*;
+import java.util.Arrays;
+public class NewSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
+		 
+        // Calling the sort() method present
+        // inside Arrays class
+        Arrays.sort(arr);
+ 
+        // Printing and display sorted array
+        System.out.printf("Modified arr[] : %s",
+                          Arrays.toString(arr));// TODO Auto-generated method stub
+
+	}
+
+}
